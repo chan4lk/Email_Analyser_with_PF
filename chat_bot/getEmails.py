97abd28ff91d6ebe7fc4de32969cf4bf9bea04c9
bhashitha dhananjaya <bhashithadhananjaya@gmail.com>
2023-10-23 01:03:55 +0530

    changed the python file and populate a new json

--- chat_bot/getEmails.py
+++ chat_bot/getEmails.py
@@ -1,74 +1,41 @@
 import json
-from nltk import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-from nltk.probability import FreqDist
-from nltk.tokenize.treebank import TreebankWordDetokenizer
-from nltk.tokenize import RegexpTokenizer
 from bs4 import BeautifulSoup
 from promptflow import tool
-
-important_keywords = ["urgent", "important", "critical"]
-
-email_bodies = []
-
-with open("simpleData.json", "r") as input_file:
-    data = json.load(input_file)
-
-    email_bodies = [
-        item.get("body", "") for item in data if isinstance(item, dict)
-    ]
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from textblob import TextBlob
 
 @tool
-def generate_email_summary():
-    summaries = []
-    for i, email_body in enumerate(email_bodies):
-        soup = BeautifulSoup(email_body, "html.parser")
-        plain_text = soup.get_text()
+def process_json_with_sentiment_analysis():
+    with open('data.json', 'r') as file:
+        json_data = json.load(file)
 
-        sentences = sent_tokenize(plain_text)
+    contents = [item['body']['content'] for item in json_data['value']]
 
-        tokenizer = RegexpTokenizer(r"\w+")
-        words = [
-            word.lower() for sentence in sentences for word in tokenizer.tokenize(sentence)
-        ]
+    def convert_html_to_text(html):
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup.get_text()
 
-        stop_words = set(stopwords.words("english"))
-        filtered_words = [word for word in words if word not in stop_words]
+    plain_texts = [convert_html_to_text(content) if any(tag for tag in BeautifulSoup(content, 'html.parser').find_all()) else content for content in contents]
 
-        word_freq = FreqDist(filtered_words)
+    sid = SentimentIntensityAnalyzer()
 
-        sentence_scores = {}
-        for sentence in sentences:
-            for word in tokenizer.tokenize(sentence):
-                if word.lower() in word_freq:
-                    if sentence not in sentence_scores:
-                        sentence_scores[sentence] = word_freq[word.lower()]
-                    else:
-                        sentence_scores[sentence] += word_freq[word.lower()]
+    analyses = []
 
-        summary_sentences = sorted(sentence_scores, key=sentence_scores.get, reverse=True)[:5]
+    for text in plain_texts:
+        sentiment_scores = sid.polarity_scores(text)
+        sentiment = "Positive" if sentiment_scores['compound'] > 0 else "Negative" if sentiment_scores['compound'] < 0 else "Neutral"
 
-        summary = TreebankWordDetokenizer().detokenize(summary_sentences)
-        
-        is_important = any(keyword in plain_text.lower() for keyword in important_keywords)
-        if is_important:
-            feedback = "Important"
-        else:
-            feedback = "Not Important"
+        text_blob = TextBlob(text)
+        subjectivity = text_blob.sentiment.subjectivity
 
-        summaries.append((summary, feedback))
-        print(f"Summary for Email {i + 1}:")
-        print(summary)
-        print(f"Email {i + 1} is: {feedback}")
+        analysis = {
+            "content": text,
+            "Sentiment": sentiment,
+            "Subjectivity": subjectivity
+        }
+        analyses.append(analysis)
 
-    return summaries
+    return analyses
 
-def chat_bot(input_text):
-    if "how is my emails today" in input_text.lower():
-        summaries = generate_email_summary()
-        return summaries
-    else:
-        return "I'm sorry, I cannot provide information"
-
-input_text = "How is my emails today?"
-response = chat_bot(input_text)
+result = process_json_with_sentiment_analysis()
+print(json.dumps(result, indent=2))
